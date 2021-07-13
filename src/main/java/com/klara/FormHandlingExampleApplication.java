316ed1ac1efa2c3bc79b64ea93c2321e1b3a1ac5
klara333 <klara.jurasova@gmail.com>
2021-07-13 12:00:45 +0200

    Add files via upload

---
+++ src/main/java/com/klara/FormHandlingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.klara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormHandlingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormHandlingExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klara/FormHandlingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormHandlingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
